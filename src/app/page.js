8749b23b7af98aee9b0d704fe07b165d0e1ad249
Joel <joel.boynton@ibm.com>
2026-05-29 12:11:43 -0400

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
+'use client';
+import { Button } from '@carbon/react';
 
 export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
+  return <Button>Button</Button>;
+}
 
-  )
+import LandingPage from './home/page';
+
+export function Page() {
+  return <LandingPage />;
+}
+
+import RepoPage from './repos/page';
+export function Repo() {
+  return <RepoPage />;
 }
